25e75b011cd5a25b9d3291c6d5a57ebd99251c5f
Stephan Ullmann <stephan.ullmann@posteo.de>
2025-09-11 16:02:40 +0200

    timer Korrekturen

--- 03_Timers-Korrekturen/01-schedule-fetch/script.js
+++ 03_Timers-Korrekturen/01-schedule-fetch/script.js
@@ -1,1 +1,75 @@
-//
+let counter = 1;
+
+const fetchPokemon = async (counter) => {
+  try {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${counter}`);
+    const data = await response.json();
+
+    console.log('Pokemon name: ', data.name);
+    console.log('Pokemon ID: ', data.id);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const intervalID = setInterval(() => {
+  fetchPokemon(counter);
+  counter++;
+  if (counter > 15) clearInterval(intervalID);
+}, 1000);
+
+// // setTimeout(() => {
+// //   clearInterval(intervalID);
+// // }, 150_000);
+
+// // let counter = 1;
+
+// // const interval = setInterval(async () => {
+// //   try {
+// //     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${counter}`);
+// //     const data = await response.json();
+
+// //     console.log({
+// //       name: data.name,
+// //       id: data.id,
+// //     });
+// //     counter++;
+// //   } catch (error) {
+// //     console.error('Fehler beim Abrufen:', error);
+// //   }
+// //   if (counter > 150) {
+// //     clearInterval(interval);
+// //     console.log('Alle 150 Original-Pokémon wurden geladen!');
+// //   }
+// // }, 1000);
+
+// let counter = Math.floor(Math.random() * 1000) + 1;
+// const showPokemon = (pokemon) => {
+//   console.log(pokemon);
+// };
+// const fetchPokemon = async () => {
+//   try {
+//     console.log('try to fetch a pokemon...');
+//     const url = `https://pokeapi.co/api/v2/pokemon/${counter}`;
+//     const res = await fetch(url);
+//     const data = await res.json();
+
+//     const pokemon = {
+//       name: data.name,
+//       id: data.id,
+//     };
+//     showPokemon(pokemon);
+//   } catch (error) {
+//     console.log(error);
+//   }
+//   counter++;
+// };
+// const fiveRequests = () => {
+//   fetchPokemon();
+// };
+
+// const intervalID = setInterval(fiveRequests, 1000);
+
+// setTimeout(() => {
+//   clearInterval(intervalID);
+// }, 5000);
--- 03_Timers-Korrekturen/02-schedule-DOM-element/script.js
+++ 03_Timers-Korrekturen/02-schedule-DOM-element/script.js
@@ -1,1 +1,31 @@
-//
+const messageContainer = document.getElementById('message-container');
+
+const addMessage = () => {
+  const html = `<div class="px-5 py-3 rounded bg-red-300">
+            <span class="text-center">Special Offer: Get 20% off your next purchase!</span>
+          </div>`;
+
+  messageContainer.insertAdjacentHTML('beforeend', html);
+};
+
+setTimeout(addMessage, 3000);
+
+// const container = document.getElementById('message-container');
+
+// const timeoutId1 = setTimeout(() => {
+//   const div = document.createElement('div');
+//   div.style.cssText = `border: 4px solid red; padding: 20px; background: hotpink`;
+//   div.classList.add('border', 'rounded-xl', 'text-center');
+//   div.innerHTML = 'Tante Hansen kommt heute';
+//   container.insertAdjacentElement('beforeend', div);
+// }, 2000);
+
+// const MsgContainer = document.getElementById('message-container');
+
+// const element = document.createElement('div');
+// element.textContent = 'Special Offer: Get 20% off your next purchase!';
+// element.classList.add('bg-red-400', 'text-white', 'font-bold', 'px-5', 'py-3', 'rounded', 'text-center');
+
+// setTimeout(() => {
+//   MsgContainer.appendChild(element);
+// }, 3000);
